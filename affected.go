171d822c6f1938a9bc6b244fad64dbd002753c91
Chris Reeves <chris.reeves@vidsy.co>
2019-12-03 17:22:39 +0000

    feat: support grouping packages

--- affected.go
+++ affected.go
@@ -1,7 +1,10 @@
 package affected
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/vidsy/affected/module"
 	"github.com/vidsy/affected/vcs"
@@ -12,6 +15,86 @@ import (
 type Cause struct {
 	Package    *module.Package   // The package that has modififcations
 	ImportPath module.ImportPath // The import graph to that package
+}
+
+// A Group holds a group of affected packages
+type Group struct {
+	Name     string
+	Packages []Package
+	Causes   []Cause
+}
+
+// MarshalJSON marshales groups of affected packages to json
+func (g Group) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"group":    g.Name,
+		"packages": g.Packages,
+	})
+}
+
+// Groups holds groups of grouped affected packages
+type Groups []Group
+
+func (g Groups) String() string {
+	w := new(bytes.Buffer)
+
+	for n, group := range g {
+		if n > 0 {
+			fmt.Fprint(w, "\n")
+		}
+
+		title := fmt.Sprintf("Group: %s", group.Name)
+
+		fmt.Fprintln(w, title)
+		fmt.Fprintln(w, strings.Repeat("-", len(title)))
+
+		for _, pkg := range group.Packages {
+			fmt.Fprintln(w, "- Package: ", pkg.ID)
+
+			for _, cause := range pkg.Causes {
+				fmt.Fprintln(w, " - Caused By:", cause.Package.ID)
+
+				for i, pkg := range cause.ImportPath {
+					fmt.Fprintln(w, fmt.Sprintf("  %s %s", strings.Repeat(">", i), pkg.ID))
+				}
+			}
+		}
+	}
+
+	return w.String()
+}
+
+// A GroupFunc determines a packages group name and if it should be grouped
+type GroupFunc func(*Package) (string, bool)
+
+// GroupPackages groups affected packages into groups determined by the GroupFunc
+func GroupPackages(fn GroupFunc, pkgs ...Package) Groups {
+	gm := make(map[string]*Group)
+
+	for _, pkg := range pkgs {
+		pkg := pkg
+
+		if name, ok := fn(&pkg); ok {
+			g, ok := gm[name]
+			if !ok {
+				g = &Group{
+					Name: name,
+				}
+
+				gm[name] = g
+			}
+
+			g.Packages = append(g.Packages, pkg)
+			g.Causes = append(g.Causes, pkg.Causes...)
+		}
+	}
+
+	groups := make(Groups, 0, len(gm))
+	for _, g := range gm {
+		groups = append(groups, *g)
+	}
+
+	return groups
 }
 
 // Package represents a modified package
@@ -54,7 +137,7 @@ func NoParents(p *module.Package) bool {
 }
 
 // Packages returns packages affected by two different commits either directly or indirectly
-func Packages(mod, a, b string, fn SubjectFunc) ([]*Package, error) {
+func Packages(mod, a, b string) ([]Package, error) {
 	g, err := git.New()
 	if err != nil {
 		return nil, err
@@ -82,31 +165,29 @@ func Packages(mod, a, b string, fn SubjectFunc) ([]*Package, error) {
 	for _, dir := range dirs {
 		if modified := graph.Find(module.FindPackageByDir(dir)); modified != nil {
 			for pkg := range graph {
-				if fn(pkg) {
-					path := graph.ImportPath(pkg, modified)
-					if len(path) > 0 {
-						affected, ok := m[pkg.ID]
-						if !ok {
-							affected = &Package{
-								Package: pkg,
-							}
-
-							m[pkg.ID] = affected
+				path := graph.ImportPath(pkg, modified)
+				if len(path) > 0 {
+					affected, ok := m[pkg.ID]
+					if !ok {
+						affected = &Package{
+							Package: pkg,
 						}
 
-						affected.Causes = append(affected.Causes, Cause{
-							Package:    modified,
-							ImportPath: path,
-						})
+						m[pkg.ID] = affected
 					}
+
+					affected.Causes = append(affected.Causes, Cause{
+						Package:    modified,
+						ImportPath: path,
+					})
 				}
 			}
 		}
 	}
 
-	affected := make([]*Package, 0, len(m))
+	affected := make([]Package, 0, len(m))
 	for _, pkg := range m {
-		affected = append(affected, pkg)
+		affected = append(affected, *pkg)
 	}
 
 	return affected, nil
