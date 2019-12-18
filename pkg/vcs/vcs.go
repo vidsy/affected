85bf3a0e871557e5e3bf5086fb2268288242f732
Chris Reeves <chris.reeves@vidsy.co>
2019-12-18 15:39:53 +0000

    feat(vcs): support globbing in vcs implementations

--- pkg/vcs/vcs.go
+++ pkg/vcs/vcs.go
@@ -1,10 +1,29 @@
 package vcs
 
-import "github.com/vidsy/affected/pkg/vcs/git"
+// ModifiedDirectoriesOptions holds optional configuration for returning modified directories
+type ModifiedDirectoriesOptions struct {
+	IncludeGlobs []string
+	ExcludeGlobs []string
+}
 
-var _ ModifiedDirectoriesDetector = new(git.VCS)
+// ModifiedDirectoriesOption updates ModifiedDirectoriesOptions
+type ModifiedDirectoriesOption func(*ModifiedDirectoriesOptions)
+
+// ModifiedDirectoriesIncludeGlobs sets the include globs
+func ModifiedDirectoriesIncludeGlobs(globs ...string) ModifiedDirectoriesOption {
+	return func(opts *ModifiedDirectoriesOptions) {
+		opts.IncludeGlobs = globs
+	}
+}
+
+// ModifiedDirectoriesExcludeGlobs sets the exclude globs
+func ModifiedDirectoriesExcludeGlobs(globs ...string) ModifiedDirectoriesOption {
+	return func(opts *ModifiedDirectoriesOptions) {
+		opts.ExcludeGlobs = globs
+	}
+}
 
 // A ModifiedDirectoriesDetector can detect modified directories
 type ModifiedDirectoriesDetector interface {
-	ModifiedDirectories(a, b string) ([]string, error)
+	ModifiedDirectories(a, b string, opts ...ModifiedDirectoriesOption) ([]string, error)
 }
